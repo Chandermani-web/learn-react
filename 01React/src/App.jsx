57d3c6beb47c7bce8900bcceb33cda14f2eca48d
Chandermani-web <chandermanimishra91.in>
2025-03-13 20:16:51 +0530

    first day of learning react

--- 01React/src/App.jsx
+++ 01React/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
     <div>
-      {["Ogo","Home","About","Service"]}
+      <div className="navbar">
+        <h1 id="logo">OGO</h1>
+        <ul className='nav-links'>
+          {["home","about","service","contact","signup/login"].map((item, index, array)=>{
+            return (
+              <li key={index} id={item.toLowerCase().replace("/","-")} className={index == array.length-1 ? "registration": ""}>
+              {item}
+              </li>
+            )            
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
